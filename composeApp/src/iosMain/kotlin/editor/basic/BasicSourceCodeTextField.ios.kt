a5c0d38761b7b36526472c5880b8cd82b5e98d04
Evgeniy.Zhelenskiy <zhelenskiy2000@yandex.ru>
2024-01-22 00:48:10 +0100

    Enhance color, focus and scrolling customizations

--- composeApp/src/iosMain/kotlin/editor/basic/BasicSourceCodeTextField.ios.kt
+++ composeApp/src/iosMain/kotlin/editor/basic/BasicSourceCodeTextField.ios.kt
@@ -1,11 +1,18 @@
 package editor.basic
 
 import androidx.compose.foundation.ScrollState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.IntOffset
 import kotlinx.coroutines.CoroutineScope
 
 actual fun Modifier.scrollOnPress(
     coroutineScope: CoroutineScope,
     verticalScrollState: ScrollState,
     horizontalScrollState: ScrollState
+): Modifier = this
+
+@Composable
+actual fun Modifier.tooltip(
+    onOffsetChange: (IntOffset) -> Unit
 ): Modifier = this
